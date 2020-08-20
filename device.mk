147ec3a347cdb1fd5dde04bd6847190516514a51
lupesoltec <dev@lupesoltec.com.br>
2020-08-20 19:01:47 -0400

    daisy: parts: Introduce HAL3|HAL1 Switch
    Introducing a HAL switch so users can easily swap between HAL1 and HAL3 without root and or having to reboot
    
    @TogoFire Adapted: for XiaomiParts
    @lupesoltec Adapted: for "Lineage" parts
    
    This implementation is based / inspired by:
    TogoFire/device_xiaomi_daisy@4335553
    
    Change-Id: 435e53071c943d33df74de5f3db6f09e22633bb1
    
    Co-Authored-By: TogoFire <italomellopereira@gmail.com>
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- device.mk
+++ device.mk
@@ -372,7 +372,9 @@ PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     move_time_data.sh \
     move_wifi_data.sh \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.camera.rc \
+    init.camera.sh
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
